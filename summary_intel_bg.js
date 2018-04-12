ec954acef337be8e423da2ccc3b3d7efdc6241a3
DC0 <readacted@readacted>
2018-04-12 17:31:37 +0800

    OK

---
+++ summary_intel_bg.js
@@ -0,0 +1,27 @@
+"use strict";
+{
+  const summaryIntel = {
+    countPublicPhotosTagged,
+    countPublicPhotosLiked,
+    countPublicStoriesTagged,
+    countPublicStoriesLiked
+  };
+
+  Object.assign(self, summaryIntel);
+
+  function countPublicPhotosTagged(id) {
+
+  }
+
+  function countPublicPhotosLiked(id) {
+
+  }
+
+  function countPublicStoriesTagged(id) {
+
+  }
+
+  function countPublicStoriesLiked(id) {
+
+  }
+}
