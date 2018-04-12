07ae3b4f54c0f243a6c57a09b2e7db0ff29c9f69
DC0 <readacted@readacted>
2018-04-12 19:56:56 +0800

    OK

--- summary_intel_bg.js
+++ summary_intel_bg.js
@@ -8,38 +8,63 @@
   };
 
   const newTab = promisify((...args) => chrome.tabs.create(...args));
+  const exe = promisify((...args) => chrome.tabs.executeScript(...args));
 
   Object.assign(self, summaryIntel);
 
-  function countPublicPhotosTagged(id) {
+  async function countPublicPhotosTagged(id) {
     const tab = await newTab({
       active: false,
       index: 1000,
-      url: 'https://www.facebook.com/search/${id}/photos-tagged/intersect'
+      url: `https://www.facebook.com/search/${id}/photos-tagged/intersect`
+    });
+    const result = await exe(tab.id, {
+      code: `
+        await new Promise((res,rej) => {
+          let count = 0;
+          const int = setInterval(() => {
+            scrollTo(0,scrollY+1000);
+            const storyStamps = Array.from(document.querySelectorAll('.timestampContent'));
+            const photoLinks = Array.from(document.querySelectorAll('a[rel="theater"]'));
+            let newCount;
+            if ( !! storyStamps ) {
+              newCount = storyStamps;
+            } else if ( !! photoLinks ) {
+              newCount = photoLinks / 2;
+            }
+            if ( newCount == count ) {
+              console.log(count);
+              res(count);    
+            }
+          }, 1000);
+        });
+      `
+    });
+    console.log(result);     
+    return result;
+  }
+
+  async function countPublicPhotosLiked(id) {
+    const tab = await newTab({
+      active: false,
+      index: 1000,
+      url: `https://www.facebook.com/search/${id}/photos-liked/intersect`
     });
   }
 
-  function countPublicPhotosLiked(id) {
+  async function countPublicStoriesTagged(id) {
     const tab = await newTab({
       active: false,
       index: 1000,
-      url: 'https://www.facebook.com/search/${id}/photos-liked/intersect'
+      url: `https://www.facebook.com/search/${id}/stories-tagged/intersect`
     });
   }
 
-  function countPublicStoriesTagged(id) {
+  async function countPublicStoriesLiked(id) {
     const tab = await newTab({
       active: false,
       index: 1000,
-      url: 'https://www.facebook.com/search/${id}/stories-tagged/intersect'
-    });
-  }
-
-  function countPublicStoriesLiked(id) {
-    const tab = await newTab({
-      active: false,
-      index: 1000,
-      url: 'https://www.facebook.com/search/${id}/stories-liked/intersect'
+      url: `https://www.facebook.com/search/${id}/stories-liked/intersect`
     });
   }
 }
