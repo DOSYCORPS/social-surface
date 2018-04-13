89a5cce5c2c05adfa8576fdff4979f9ea45863f5
DC0 <readacted@readacted>
2018-04-13 15:53:36 +0800

    OK

--- getId.js
+++ getId.js
@@ -1,14 +1,22 @@
 "use strict";
 {
   Object.assign( self, {getId} );
+  let found = false;
 
   // gets the facebook id from https://www.facebook.com
+  
+  const int = setInterval( () => {
+    if ( found ) clearInterval(int);
+    getId();
+  }, 500 );
 
   function getId() {
+    if ( found ) return;
     const userNavItem = document.querySelector("[data-nav-item-id]");
-    if ( !! userNavItem ) {
+    if ( ! userNavItem ) {
       throw new TypeError("Cannot get user id");      
     }
-    return userNavItem.dataset.navItemId;
+    found = true;
+    chrome.extension.sendMessage({ type: 'idFound', id : userNavItem.dataset.navItemId});
   }
 }
