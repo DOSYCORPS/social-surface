cee015ed8a20a70026ca493675fa990a778addcf
DC0 <readacted@readacted>
2018-04-13 15:46:52 +0800

    OK

--- run_bg.js
+++ run_bg.js
@@ -3,6 +3,6 @@
   chrome.tabs.create({active:true,url: chrome.extension.getURL('ui.html')});
 
   chrome.runtime.onMessage.addListener( (msg, sender, reply) => {
-    console.log(msg);
+    console.log("MSG", msg);
   });
 }
---
+++ ui_app.js
@@ -0,0 +1,30 @@
+"use strict";
+{
+  const table = document.querySelector('table');
+  table.addEventListener('onclick', e => {
+    const target = e.target;
+    if ( target.matches('button.run') {
+      chrome.tabs.create({url:'https://www.facebook.com/',active:true});
+    } else if ( target.matches('button.fix') ) {
+      alert("Fix advice.");
+    }
+  });
+
+  let id;
+
+  chrome.runtime.onMessage.addListener( (msg, sender, reply) => {
+    if ( msg.type == 'idFound' ) {
+      id = msg.id; 
+      countPublicPhotosTagged(id);
+      countPublicPhotosLiked(id);
+      countPublicStoriesTagged(id);
+      countPublicStoriesLiked(id);
+    } else if ( msg.type == 'countUpdate' ) {
+      const countEl = document.querySelector(`#${msg.countType} .count`);
+      countEl.innerText = msg.count;
+      if ( !! msg.done ) {
+        document.querySelector(`#${msg.countType} button.fix`).disabled = false;
+      }
+    }
+  });
+}
